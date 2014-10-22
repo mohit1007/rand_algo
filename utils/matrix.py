7b143b26c49d326fafa40c537ceeb587ef6232f1
Mohit Singh <msingh@msingh-macbookpro-2.local>
2014-10-22 00:24:05 -0700

    base matrix usage and some raw utils file

--- utils/matrix.py
+++ utils/matrix.py
@@ -1,5 +1,31 @@
 __author__ = 'msingh'
 
+from utils import *
+
+
 class Matrix(object):
     def __init__(self, rdd):
-        self.rdd = rdd
+        self.matrix = rdd
+
+    def transpose(self):
+        rows_each_part = self.matrix.mapPartitions(lambda x: num_rows_each_partition(x)).collect()
+        indexed_matrix = self.matrix.mapPartitionsWithSplit(lambda index, it: indexing(index, it, rows_each_part)). \
+            flatMap(lambda x: flip(x))
+        grouped = indexed_matrix.groupBy(lambda x: x[0]).map(lambda x: [e for e in x[1]])
+        return Matrix(grouped.map(lambda x: extract(x)))
+
+
+    def dot(self, other):
+        # join by columns and sum the corresponding eles
+        return Matrix(self.matrix.join(other.matrix).map(lambda (k, v): v). \
+                      mapPartitions(matrixsum_iterator_other).sum())
+
+    def take(self, rows):
+        return self.matrix.matrix.take(rows)
+
+
+    def top(self):
+        return self.matrix.matrix.first()
+
+    def collect(self):
+        return self.matrix.collect()
---
+++ utils/utils.py
@@ -0,0 +1,31 @@
+__author__ = 'msingh'
+
+import numpy as np
+
+
+def num_rows_each_partition(iterator):
+    yield sum(1 for _ in iterator)
+
+
+def indexing(splitIndex, iterator, count_each_partition):
+    # count = 0
+    offset = sum(count_each_partition[:splitIndex]) if splitIndex else 0
+    indexed = []
+    for i, e in enumerate(iterator):
+        index = offset + i
+        for j, ele in enumerate(e):
+            indexed.append((index, j, ele))
+    yield indexed
+
+
+def flip(row):
+    return [(e[1], e[0], e[2]) for e in row]
+
+
+def extract(row):
+    return [ele[2] for ele in row]
+
+
+def matrixsum_iterator_other(iterator):
+    yield sum(np.outer(x, y) for x, y in iterator)
+
