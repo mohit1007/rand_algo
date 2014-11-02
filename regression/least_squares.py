111ca0304b62943e28a49d5bb2ad528433b3ecab
mohit.singh <mohit.singh@rd.io>
2014-11-02 00:04:18 -0700

    minor tweaks

--- regression/least_squares.py
+++ regression/least_squares.py
@@ -1,7 +1,11 @@
 from rand_solvers.projections import *
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 class RandLeastSquares:
-    def __init__(self,matrix, **kwargs):
+    def __init__(self, matrix, **kwargs):
         self.k = kwargs.get('k')
         self.projection = Projections(matrix, **kwargs)
         self.cost = []
@@ -10,13 +14,13 @@ class RandLeastSquares:
 
         l2_proj = self.projection.execute()
         if debug:
-            x_opt, f_opt = self.__ideal_cost(X,y)
+            x_opt, f_opt = self.__ideal_cost(X, y)
 
         for i in range(self.k):
-            cost =  npl.norm(np.dot(X, l2_proj[i])-y)
+            cost = npl.norm(np.dot(X, l2_proj[i])-y)
             if debug:
-                print npl.norm(np.array(l2_proj[i])-x_opt)/npl.norm(x_opt)
-                print np.abs(f_opt-cost)/f_opt
+                logger.info(npl.norm(np.array(l2_proj[i])-x_opt)/npl.norm(x_opt))
+                logger.info(np.abs(f_opt-cost)/f_opt)
 
             self.cost.append(cost)
 
@@ -26,9 +30,9 @@ class RandLeastSquares:
     def __projection(self):
         return self.projection.execute()
 
-    def __ideal_cost(self,A,b):
-        x_opt = npl.lstsq(A,b)[0]
-        f_opt = npl.norm(np.dot(A,x_opt)-b)
+    def __ideal_cost(self, A, b):
+        x_opt = npl.lstsq(A, b)[0]
+        f_opt = npl.norm(np.dot(A, x_opt)-b)
         return x_opt, f_opt
 
 
